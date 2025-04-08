807dcddc42337108fed3c06d883c7cc09a58453d
Suraj Ghanshyam Maurya <suraj.maurya@amoga.io>
2025-04-09 00:25:17 +0530

    Updated Basicsday1.js and added index.html

---
+++ Basicsday1.js
@@ -0,0 +1,15 @@
+firstName = "Suraj"
+
+console.log(typeof firstName)
+
+
+firstName = 6
+console.log(typeof firstName)
+
+
+firstName = null
+console.log(typeof firstName)
+
+firstName = true
+console.log(typeof firstName)
+
